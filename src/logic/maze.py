24a1579dde6767fdf7c98e5db112b74bd777c4a0
Mustapha Nkhili <mustapha.developer.23@gmail.com>
2026-09-23 16:21:28 +0000

    feat(maze): add load_maze helper

---
+++ src/logic/maze.py
@@ -0,0 +1,10 @@
+from mazegenerator import MazeGenerator
+
+def load_maze(width: int = 15, height: int = 15, seed: int = 0, perfect: bool = False):
+    generator = MazeGenerator(size=(width, height), seed=seed, perfect=perfect)
+
+    return {
+        "maze": generator.maze,
+        "width": width,
+        "height": height
+    }

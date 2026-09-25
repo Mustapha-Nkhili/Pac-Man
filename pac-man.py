0dc6f28baef56557a7651542fa378e4fce70b69b
Mustapha Nkhili <mustapha.developer.23@gmail.com>
2026-09-25 15:23:44 +0000

    feat: integrate config and maze loading into main

--- pac-man.py
+++ pac-man.py
@@ -1,5 +1,20 @@
+import sys
+
+from src.config_loader import load_config
+from src.logic.maze import load_maze
+
 def main():
-    print("hello Pac-Man")
+    if len(sys.argv) != 2:
+        print("Error: Invalid number of arguments.", file=sys.stderr)
+        print(f"Usage: python {sys.argv[0]} config.json", file=sys.stderr)
+        sys.exit(1)
+
+    config = load_config(sys.argv[1])
+    maze = load_maze(config["level"][0]["width"],
+                     config["level"][0]["height"],
+                     config["seed"])
+    print(maze)
+
 
 if __name__ == "__main__":
     main()
